5adfb967345773999e305a130a613374b63e76ab
Thomas <thom.cherryhomes@gmail.com>
2022-11-13 13:06:06 -0600

    final.

--- src/apx.asm
+++ src/apx.asm
@@ -25,6 +25,7 @@ VDSLSTH =	$0201
 
 GRACTL	=	$D01D
 PMBASE  =	$D407
+CHBASE	=	$D409
 WSYNC	=	$D40A
 VCOUNT  =       $D40B
 NMIEN	=	$D40E
@@ -109,7 +110,7 @@ PMG:				; P/M Data
 	
 DLST:	.BYTE	$F0,$70,$70,$70,$70,$70,$70
 	.BYTE	$47,.LO(SCR),.HI(SCR)
-	.BYTE	$07,$07,$07,$07
+	.BYTE	$07,$07,$07,$87,$07,$06
 	.BYTE	$41,.LO(DLST),.HI(DLST)
 
 	;; Screen Data
@@ -119,57 +120,104 @@ SCR:
 	.BYTE $1a,$04,$1b,$1c,$1d,$04,$1e,$04,$1f,$20,$21,$22,$23,$24,$25,$00,$bc,$fc,$7c,$00
 	.BYTE $26,$04,$04,$04,$04,$04,$1e,$04,$27,$28,$29,$2a,$2b,$2c,$2d,$2e,$bc,$fc,$7c,$00
 	.BYTE $2f,$30,$31,$32,$33,$04,$1e,$04,$34,$00,$35,$36,$37,$38,$39,$3a,$bd,$fd,$7d,$00
+	.BYTE "  PROGRAM DATABASE  "
+	.BYTE "      FOR #FUJINET  "
 
+	;; This DLI attempted to reposition the blue bar to the right. Didn't work, but
+	;; am leaving it here for posterity.
+	
+;; DLI:	PHA
+;; L0:	STA WSYNC
+;; 	LDA #$AC
+;; 	STA $D000
+;; 	LDA VCOUNT
+;; 	AND #$07
+;; 	BEQ L2
+;; L1:     NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP	
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	JMP LC
+;; L2:	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; 	NOP
+;; LC:	LDA #$C8
+;; 	STA $D000
+;; 	LDA VCOUNT
+;; 	CMP #80
+;; 	BNE L0
+;; 	PLA
+;; 	RTI
+
+	;; Alternate DLI which flips back to normal charset
 DLI:	PHA
-L0:	STA WSYNC
-	LDA #$AC
-	STA $D000
-	LDA VCOUNT
-	AND #$07
-	BEQ L2
-L1:     NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP	
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	JMP LC
-L2:	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-	NOP
-LC:	LDA #$C8
-	STA $D000
-	LDA VCOUNT
-	CMP #80
-	BNE L0
+	STA WSYNC
+	LDA #$E0
+	STA CHBASE
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	STA WSYNC
+	LDA #.hi(FONT)
+	STA CHBASE
 	PLA
 	RTI
 	
@@ -242,11 +290,6 @@ SL1:	LDA	$02C4,X
 	LDA	#$C4
 	STA	$D003
 
-	;; LDA	#$CD
-	;; STA	$D004
-	;; LDA	#$CE
-	;; STA	$D005
-	
 	;; SET DISPLAY LIST
 
 	LDA	#.lo(DLST)
